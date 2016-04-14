881e680c021b5a85035676c02d40613e88941a58
123 <1145565903@qq.com>
2016-04-14 15:00:13 +0800

    ABC

---
+++ LazyWeekend/LazyWeekend/View/LWSpaceView.m
@@ -0,0 +1,21 @@
+//
+//  LWSpaceView.m
+//  LazyWeekend
+//
+//  Created by tens03 on 16/4/14.
+//  Copyright © 2016年 tens03. All rights reserved.
+//
+
+#import "LWSpaceView.h"
+
+@implementation LWSpaceView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
